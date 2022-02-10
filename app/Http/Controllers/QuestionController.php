90d3c784190621d43c3498f494dbc3d13c39c02d
pradeep-vmoksha <70131623+pradeep-vmoksha@users.noreply.github.com>
2022-02-10 10:24:55 +0530

    added crud operations

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Question;
+use Exception;
 
 class QuestionController extends Controller
 {
@@ -11,6 +12,11 @@ class QuestionController extends Controller
     public function postques()
     {
         return view('postques');
+    }
+
+
+    public function edit($id){
+        return view('');
     }
 
 
@@ -31,5 +37,31 @@ class QuestionController extends Controller
         }
 
         return redirect()->back()->with('success', 'Questions are created successfully');
+    }
+
+
+    public function update($id, Request $request){
+        $data = $request->validate([
+            "title"    => "required|string|max:150",
+            "answer" => 'required|string|max:550'
+        ]);
+
+        try {
+            $update = Question::where('id', $id)->update([
+                'title' => $request->title,
+                "body" => $request->answer
+            ]);
+
+            return redirect()->back()->with('success', "Question is updated successful");
+        } catch (Exception $ex) {
+            return redirect()->back()->with('error', "Unable to updated the question. Please try again.");
+        }
+    }
+
+
+    public function destroy($id){
+        $d = Question::findOrFail($id);
+        $d->delete();
+        return redirect('')->back()->with('success', "Question is deleted successful");
     }
 }
